5cb3ffea58819b20e02e7f154033562c655c8480
mustby <mjustby@gmail.com>
2025-11-08 15:28:42 -0600

    Improving Test Coverage

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -2,7 +2,10 @@
 
 pragma solidity ^0.8.24;
 
-import {Test, console} from "lib/forge-std/src/Test.sol";
+import {Test, console2} from "lib/forge-std/src/Test.sol";
+
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IAccessControl} from "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 
 import {RebaseToken} from "src/RebaseToken.sol";
 import {Vault} from "src/Vault.sol";
@@ -21,8 +24,11 @@ contract RebaseTokenTest is Test {
         rebaseToken = new RebaseToken(); // deploying rebase token
         vault = new Vault(IRebaseToken(address(rebaseToken))); // redploying vault with rebase token address
         rebaseToken.grantMintAndBurnRole(address(vault)); // granting mint and burn role to vault
-        (bool success,) = payable(address(vault)).call{value: 1e18}(""); // Adding some funds to the vault - 1 ETH
         vm.stopPrank();
+    }
+
+    function addRewardsToVault(uint256 rewardAmount) public {
+        (bool success,) = payable(address(vault)).call{value: rewardAmount}(""); // Adding some funds to the vault - 1 ETH
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -38,7 +44,7 @@ contract RebaseTokenTest is Test {
         vault.deposit{value: amount}();
         // 2. check out rebase token balance
         uint256 startBalance = rebaseToken.balanceOf(user);
-        console.log("startBalance:", startBalance);
+        console2.log("startBalance:", startBalance);
         assertEq(startBalance, amount);
         // 3. warp the time and check the balance again
         vm.warp(block.timestamp + 1 hours);
@@ -67,5 +73,111 @@ contract RebaseTokenTest is Test {
         vm.stopPrank();
     }
 
-    function testInterestRateCanOnlyGoDown() public {}
+    function testRedeemAfterTimePassed(uint256 depositAmount, uint256 time) public {
+        time = bound(time, 1000, type(uint96).max);
+        depositAmount = bound(depositAmount, 1e5, type(uint96).max);
+        // 1. deposit
+        vm.deal(user, depositAmount);
+        vm.prank(user);
+        vault.deposit{value: depositAmount}();
+
+        // 2. warp the time
+        vm.warp(block.timestamp + time);
+        uint256 balanceAfterSomeTime = rebaseToken.balanceOf(user);
+        // 2.b add the rewards to the vault
+        vm.deal(owner, balanceAfterSomeTime - depositAmount);
+        vm.prank(owner);
+        addRewardsToVault(balanceAfterSomeTime - depositAmount);
+
+        // 3. redeem
+        vm.prank(user);
+        vault.redeem(type(uint256).max);
+
+        uint256 ethBalance = address(user).balance;
+
+        assertEq(ethBalance, balanceAfterSomeTime);
+        assertGt(ethBalance, depositAmount);
+    }
+
+    function testTransfer(uint256 amount, uint256 amountToSend) public {
+        amount = bound(amount, 1e5 + 1e5, type(uint96).max);
+        amountToSend = bound(amountToSend, 1e5, amount - 1e5);
+
+        // 1. deposit
+        vm.deal(user, amount);
+        vm.prank(user);
+        vault.deposit{value: amount}();
+
+        address user2 = makeAddr("user2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, amount);
+        assertEq(user2Balance, 0);
+
+        // owner reduces the interest rate
+        vm.prank(owner);
+        rebaseToken.setInterestRate(4e10); // 4%
+
+        // 2. transfer
+        vm.prank(user);
+        rebaseToken.transfer(user2, amountToSend);
+        uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+        assertEq(userBalanceAfterTransfer, userBalance - amountToSend);
+        assertEq(user2BalanceAfterTransfer, amountToSend);
+
+        // 3. Check the user interest rates (5e10 to 4e10)
+        assertEq(rebaseToken.getUserInterestRate(user), 5e10);
+        assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
+
+        // 4. Check and log the current contract interest rate
+        uint256 currentInterestRate = rebaseToken.getInterestRate();
+        console2.log("Current Interest Rate:", currentInterestRate);
+    }
+
+    function testCannotSetInterestRateByNonOwner(uint256 newInterestRate) public {
+        vm.prank(user);
+        vm.expectPartialRevert(bytes4(Ownable.OwnableUnauthorizedAccount.selector));
+        rebaseToken.setInterestRate(newInterestRate); // 4%
+    }
+
+    function testCannotCallMintAndBurnWithoutRole() public {
+        // Try to Mint
+        vm.prank(user);
+        vm.expectPartialRevert(bytes4(IAccessControl.AccessControlUnauthorizedAccount.selector));
+        rebaseToken.mint(user, 1e18, 5e10);
+
+        // Try to Burn
+        vm.prank(user);
+        vm.expectPartialRevert(bytes4(IAccessControl.AccessControlUnauthorizedAccount.selector));
+        rebaseToken.burn(user, 1e18);
+    }
+
+    function testGetPrincipleAmount(uint256 amount) public {
+        amount = bound(amount, 1e5, type(uint96).max);
+        // 1. deposit
+        vm.startPrank(user);
+        vm.deal(user, amount);
+        vault.deposit{value: amount}();
+        assertEq(rebaseToken.principleBalanceOf(user), amount);
+
+        // 2. warp the time and check the principle balance again
+        vm.warp(block.timestamp + 1 hours);
+        assertEq(rebaseToken.principleBalanceOf(user), amount);
+    }
+
+    function testGetRebaseTokenAddress() public {
+        assertEq(vault.getRebaseTokenAddress(), address(rebaseToken));
+    }
+
+    function testInterestRateCanOnlyDecrease(uint256 newInterestRate) public {
+        uint256 initialInterestRate = rebaseToken.getInterestRate();
+        newInterestRate = bound(newInterestRate, initialInterestRate, type(uint96).max);
+        vm.prank(owner);
+        vm.expectPartialRevert(bytes4(RebaseToken.RebaseToken__InterestRateCanOnlyDecrease.selector));
+        rebaseToken.setInterestRate(newInterestRate);
+        assertEq(rebaseToken.getInterestRate(), initialInterestRate);
+    }
+
+    function testNobodyCanCallMintAndBurnRole() public {}
 }
